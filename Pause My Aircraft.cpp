a6d968396b7666fb39a59c94adebfc61bda8df34
Joaquim Manjama <melorafaelmanjama@gmail.com>
2025-11-06 21:54:04 +0000

    organised the code

--- Pause My Aircraft.cpp
+++ Pause My Aircraft.cpp
@@ -1,5 +1,6 @@
 // BASED ON SAMPLE CODE FROM https://developer.x-plane.com/sdk/plugin-sdk-sample-code/
 
+// IMPORTING
 #include "XPLMDisplay.h"
 #include "XPLMGraphics.h"
 #include "XPLMDataAccess.h"
@@ -24,9 +25,12 @@
 static XPLMWindowID	g_window;
 
 // MENU
-int g_menu_container_idx; // The index of our menu item in the Plugins menu
-XPLMMenuID g_menu_id; // The menu container we'll append all our menu items to
-void menu_handler(void*, void*);
+int			g_menu_container_idx; // The index of our menu item in the Plugins menu
+XPLMMenuID	g_menu_id; // The menu container we'll append all our menu items to
+void		menu_handler(void*, void*);
+
+// WINDOWS
+void draw_manual_mode(int l, int t, int r, int b, int char_height);
 
 // Callbacks we will register when we create our window
 void				draw(XPLMWindowID in_window_id, void * in_refcon);
@@ -36,13 +40,27 @@ XPLMCursorStatus	dummy_cursor_status_handler(XPLMWindowID in_window_id, int x, i
 int					dummy_wheel_handler(XPLMWindowID in_window_id, int x, int y, int wheel, int clicks, void * in_refcon) { return 0; }
 void				dummy_key_handler(XPLMWindowID in_window_id, char key, XPLMKeyFlags flags, char virtual_key, void * in_refcon, int losing_focus) { }
 
-static const char * g_pause_label = "PAUSE";
-static float g_pause_button_lbrt[4];
+// PLUGIN MODE
+typedef enum {
+	ZULU_TIME = 0,
+	WAYPOINT = 1,
+	TOD = 2,
+	MANUAL = 3
+} PauseMode;
+static PauseMode current_mode;
+
+// PAUSE BUTTON
+static const char*	g_pause_label = "PAUSE";
+static float		g_pause_button_lbrt[4];
+
+
 
 static int	coord_in_rect(float x, float y, float* bounds_lbrt) { return ((x >= bounds_lbrt[0]) && (x < bounds_lbrt[2]) && (y < bounds_lbrt[3]) && (y >= bounds_lbrt[1])); }
 
+// PLUGIN START FUNCTION
 void pause_sim();
 
+// XPLUGIN_START
 PLUGIN_API int XPluginStart(
 							char *		outName,
 							char *		outSig,
@@ -55,7 +73,10 @@ PLUGIN_API int XPluginStart(
 	// MENU
 	g_menu_container_idx = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "Pause My Aircraft", 0, 0);
 	g_menu_id = XPLMCreateMenu("Pause My Aircraft", XPLMFindPluginsMenu(), g_menu_container_idx, menu_handler, NULL);
-	XPLMAppendMenuItem(g_menu_id, "Plugin Window", (void*)"Menu Item 1", 1);
+	XPLMAppendMenuItem(g_menu_id, "Zulu Time", (void*)"Menu Item 1", 1);
+	XPLMAppendMenuItem(g_menu_id, "Waypoint", (void*)"Menu Item 2", 1);
+	XPLMAppendMenuItem(g_menu_id, "TOD", (void*)"Menu Item 3", 1);
+	XPLMAppendMenuItem(g_menu_id, "Manual", (void*)"Menu Item 4", 1);
 
 	XPLMCreateWindow_t params;
 	params.structSize = sizeof(params);
@@ -93,9 +114,12 @@ PLUGIN_API int XPluginStart(
 	XPLMSetWindowTitle(g_window, "Pause My Aircraft");
 	XPLMSetWindowIsVisible(g_window, 0);
 
+	current_mode = MANUAL;
+
 	return g_window != NULL;
 }
 
+// XPLUGIN_STOP
 PLUGIN_API void	XPluginStop(void)
 {
 	// Since we created the window, we'll be good citizens and clean it up
@@ -106,9 +130,9 @@ PLUGIN_API void	XPluginStop(void)
 	XPLMDestroyMenu(g_menu_id);
 }
 
-PLUGIN_API void XPluginDisable(void) { }
-PLUGIN_API int  XPluginEnable(void)  { return 1; }
-PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam) { }
+PLUGIN_API void XPluginDisable(void) { } // XPLUGIN_DISABLE
+PLUGIN_API int  XPluginEnable(void)  { return 1; } // XPLUGIN_ENABLE
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam) { } // XPLUGIN_RECEIVE_MESSAGE
 
 void	draw(XPLMWindowID in_window_id, void * in_refcon)
 {
@@ -124,36 +148,16 @@ void	draw(XPLMWindowID in_window_id, void * in_refcon)
 						 0 /* no depth writing */
 						 );
 	
-	float white[] = {1.0, 1.0, 1.0}; // red, green, blue
-	float green[] = { 0.0, 1.0, 0.0, 1.0 };
-	
 	int char_height;
 	XPLMGetFontDimensions(xplmFont_Proportional, NULL, &char_height, NULL);
 
 	int l, t, r, b;
 	XPLMGetWindowGeometry(in_window_id, &l, &t, &r, &b);
 
-	// Draw Button
+	// MANUAL SCREEN
+	if (current_mode == MANUAL)
 	{
-		float pause_string_length = XPLMMeasureString(xplmFont_Proportional, g_pause_label, strlen(g_pause_label));
-
-		g_pause_button_lbrt[0] = l + ((r - l) - pause_string_length) / 2; // LEFT
-		g_pause_button_lbrt[3] = t - (((t - b) - char_height) / 2); // TOP
-		g_pause_button_lbrt[2] = g_pause_button_lbrt[0] + pause_string_length; // RIGHT
-		g_pause_button_lbrt[1] = g_pause_button_lbrt[3] - (1.25f * char_height); // BOTTOM
-
-		glColor4fv(green);
-		glBegin(GL_LINE_LOOP);
-		{
-			glVertex2i(g_pause_button_lbrt[0], g_pause_button_lbrt[3]);
-			glVertex2i(g_pause_button_lbrt[2], g_pause_button_lbrt[3]);
-			glVertex2i(g_pause_button_lbrt[2], g_pause_button_lbrt[1]);
-			glVertex2i(g_pause_button_lbrt[0], g_pause_button_lbrt[1]); 
-		}
-		glEnd();
-
-
-		XPLMDrawString(white, g_pause_button_lbrt[0], g_pause_button_lbrt[1] + 4, (char*)g_pause_label, NULL, xplmFont_Proportional);
+		draw_manual_mode(l, t, r, b, char_height);
 	}
 }
 
@@ -191,9 +195,84 @@ void menu_handler(void* in_menu_ref, void* in_item_ref)
 {
 	const char* item_name = (const char*)in_item_ref;
 
-	if (!strcmp(item_name, "Menu Item 1"))
+	XPLMSetWindowIsVisible(g_window, 1);
+	XPLMBringWindowToFront(g_window);
+
+	if (!strcmp((const char*)in_item_ref, "Menu Item 1"))
 	{
-		XPLMSetWindowIsVisible(g_window, 1);
-		XPLMBringWindowToFront(g_window);
+		current_mode = ZULU_TIME;
+	} 
+	else if (!strcmp((const char*)in_item_ref, "Menu Item 2"))
+	{
+		current_mode = WAYPOINT;
 	}
+	else if (!strcmp((const char*)in_item_ref, "Menu Item 3"))
+	{
+		current_mode = TOD;
+	}
+	else if (!strcmp((const char*)in_item_ref, "Menu Item 4"))
+	{
+		current_mode = MANUAL;
+	}
+}
+
+// DRAWING
+
+// MANUAL MODE
+void draw_manual_mode(int l, int t, int r, int b, int char_height)
+{
+	float white[] = { 1.0f, 1.0f, 1.0f };
+	float green[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+	float darkGreen[] = { 0.0f, 0.4f, 0.0f, 1.0f };
+	float shadow[] = { 0.0f, 0.0f, 0.0f, 0.4f };
+
+	// Measure text width for centering
+	float pause_string_length = XPLMMeasureString(xplmFont_Proportional, g_pause_label, strlen(g_pause_label));
+
+	// Define button bounds
+	g_pause_button_lbrt[0] = l + ((r - l) - pause_string_length - 20) / 2; // LEFT (20px padding)
+	g_pause_button_lbrt[3] = t - (((t - b) - char_height) / 2);            // TOP
+	g_pause_button_lbrt[2] = g_pause_button_lbrt[0] + pause_string_length + 20; // RIGHT
+	g_pause_button_lbrt[1] = g_pause_button_lbrt[3] - (1.8f * char_height);     // BOTTOM
+
+	// --- Draw subtle shadow behind the button ---
+	glColor4fv(shadow);
+	glBegin(GL_QUADS);
+	{
+		glVertex2i(g_pause_button_lbrt[0] + 2, g_pause_button_lbrt[3] - 2);
+		glVertex2i(g_pause_button_lbrt[2] + 2, g_pause_button_lbrt[3] - 2);
+		glVertex2i(g_pause_button_lbrt[2] + 2, g_pause_button_lbrt[1] - 2);
+		glVertex2i(g_pause_button_lbrt[0] + 2, g_pause_button_lbrt[1] - 2);
+	}
+	glEnd();
+
+	// --- Draw filled button background ---
+	glColor4f(0.0f, 0.3f, 0.0f, 0.85f); // semi-transparent dark green fill
+	glBegin(GL_QUADS);
+	{
+		glVertex2i(g_pause_button_lbrt[0], g_pause_button_lbrt[3]);
+		glVertex2i(g_pause_button_lbrt[2], g_pause_button_lbrt[3]);
+		glVertex2i(g_pause_button_lbrt[2], g_pause_button_lbrt[1]);
+		glVertex2i(g_pause_button_lbrt[0], g_pause_button_lbrt[1]);
+	}
+	glEnd();
+
+	// --- Draw bright outline for clarity ---
+	glLineWidth(2.0f);
+	glColor4fv(green);
+	glBegin(GL_LINE_LOOP);
+	{
+		glVertex2i(g_pause_button_lbrt[0], g_pause_button_lbrt[3]);
+		glVertex2i(g_pause_button_lbrt[2], g_pause_button_lbrt[3]);
+		glVertex2i(g_pause_button_lbrt[2], g_pause_button_lbrt[1]);
+		glVertex2i(g_pause_button_lbrt[0], g_pause_button_lbrt[1]);
+	}
+	glEnd();
+	glLineWidth(1.0f);
+
+	// --- Draw the pause label centered vertically and horizontally ---
+	int text_x = g_pause_button_lbrt[0] + ((g_pause_button_lbrt[2] - g_pause_button_lbrt[0]) - pause_string_length) / 2;
+	int text_y = g_pause_button_lbrt[1] + (char_height * 0.5f);
+
+	XPLMDrawString(white, text_x, text_y, (char*)g_pause_label, NULL, xplmFont_Proportional);
 }
